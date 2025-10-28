f2b8dfe66f4429ad1b48f604825e7e993a0ffa4c
ekibar <ekibar@Ubuntu-42.myguest.virtualbox.org>
2025-10-28 20:57:42 +0300

    Map copying replaced with ft_strdup

--- srcs/parser/duplicate_grid.c
+++ srcs/parser/duplicate_grid.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   duplicate_grid.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: azorlutu <azorlutu@student.42istanbul      +#+  +:+       +#+        */
+/*   By: ekibar <ekibar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 19:23:32 by azorlutu          #+#    #+#             */
-/*   Updated: 2025/10/25 19:23:32 by azorlutu         ###   ########.fr       */
+/*   Updated: 2025/10/28 20:33:40 by ekibar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 char **duplicate_grid(t_map *map)
 {
 	char	**map_copy;
-	int		y;	
+	int		y;
 	
 	map_copy = malloc(sizeof(char *) * (map->height + 1));
 	if(!map_copy)
@@ -23,13 +23,13 @@ char **duplicate_grid(t_map *map)
 	y = 0;
 	while (y < map->height)
 	{
-		map_copy[y] = map->grid[y];
-		if(!map_copy[y])
+		map_copy[y] = ft_strdup(map->grid[y]);
+		if (!map_copy[y])
 		{
-			while (y < 0)
+			while (y > 0)
 				free(map_copy[--y]);
 			free(map_copy);
-			print_error_and_exit("Map line copy error");
+			print_error_and_exit("Map line copy malloc error");
 		}
 		y++;
 	}
