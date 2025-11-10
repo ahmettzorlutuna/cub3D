cfd278380052699b09f3e89dc26e2b1085b214a8
Emine Kibar <ekibar@k1m07s01.42istanbul.com.tr>
2025-11-10 22:25:45 +0300

    refactor(texture): this commit will handle color comma space

--- srcs/engine/window.c
+++ srcs/engine/window.c
@@ -6,7 +6,7 @@
 /*   By: ekibar <ekibar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 19:11:06 by ekibar            #+#    #+#             */
-/*   Updated: 2025/11/10 11:23:50 by ekibar           ###   ########.fr       */
+/*   Updated: 2025/11/10 22:22:50 by ekibar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ void	create_window(t_game *game)
 	game->mlx.mlx_ptr = mlx_init();
 	if (!game->mlx.mlx_ptr)
 		exit_safe(game, "Mlx_init failed!", 1);
+	load_textures(game, game->map.north_texture_path, 0);
+    load_textures(game, game->map.south_texture_path, 1);
+    load_textures(game, game->map.west_texture_path, 2);
+    load_textures(game, game->map.east_texture_path, 3);
 	game->mlx.win_ptr = mlx_new_window(game->mlx.mlx_ptr, WIN_W, WIN_H,
 			"cub3D");
 	if (!game->mlx.win_ptr)
@@ -28,10 +32,6 @@ void	create_window(t_game *game)
 			&game->mlx.line_len, &game->mlx.endian);
 	if (!game->mlx.addr)
 		exit_safe(game, "Failed to get image data address\n", 1);
-	load_textures(game, game->map.north_texture_path, 0);
-    load_textures(game, game->map.south_texture_path, 1);
-    load_textures(game, game->map.west_texture_path, 2);
-    load_textures(game, game->map.east_texture_path, 3);
 	hooks_handler(game);
 	mlx_loop(game->mlx.mlx_ptr);
 }
