e9d771d9db800feb8272b83f74ab26dcff3884a6
ekibar <ekibar@Ubuntu-42.myguest.virtualbox.org>
2025-11-10 14:12:45 +0300

    init texture

--- srcs/utils/init_game.c
+++ srcs/utils/init_game.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_game.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: azorlutu <azorlutu@student.42istanbul.com  +#+  +:+       +#+        */
+/*   By: ekibar <ekibar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 22:45:56 by azorlutu          #+#    #+#             */
-/*   Updated: 2025/10/19 22:45:57 by azorlutu         ###   ########.tr       */
+/*   Updated: 2025/11/10 13:44:59 by ekibar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,13 @@ void init_game(t_game *game)
 {
     ft_memset(game, 0, sizeof(t_game));
     ft_memset(&game->mlx, 0, sizeof(t_mlx));
+    ft_memset(&game->ray, 0, sizeof(t_ray));
+    ft_memset(&game->hit, 0, sizeof(t_hit));
+    game->texture = malloc(sizeof(t_texture) * 4);
+    if (game->texture == NULL)
+        exit_safe(game, "Failed to allocate memory for textures", 1);
+    ft_memset(game->texture, 0, sizeof(t_texture) * 4);
     game->map.floor_color = -1;
     game->map.ceiling_color = -1;
     game->map.is_map_started = -1;
 }
-
