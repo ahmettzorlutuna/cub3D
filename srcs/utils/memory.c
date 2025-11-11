ab8edec26ac61bebfc8a08cfdf15a206f3211979
Ahmet Zorlutuna <ahmettayyip3439@gmail.com>
2025-11-11 21:22:30 +0300

    refactor(norm): 25 lines function left

--- srcs/utils/memory.c
+++ srcs/utils/memory.c
@@ -12,53 +12,53 @@
 
 #include "../includes/cub3d.h"
 
-void cleanup(t_game *game)
+void	cleanup(t_game *game)
 {
-    if (game->texture && game->mlx.mlx_ptr)
-    {
-        if (game->texture[0].img)
-            mlx_destroy_image(game->mlx.mlx_ptr, game->texture[0].img);
-        if (game->texture[1].img)
-            mlx_destroy_image(game->mlx.mlx_ptr, game->texture[1].img);
-        if (game->texture[2].img)
-            mlx_destroy_image(game->mlx.mlx_ptr, game->texture[2].img);
-        if (game->texture[3].img)
-            mlx_destroy_image(game->mlx.mlx_ptr, game->texture[3].img);
+	if (game->texture && game->mlx.mlx_ptr)
+	{
+		if (game->texture[0].img)
+			mlx_destroy_image(game->mlx.mlx_ptr, game->texture[0].img);
+		if (game->texture[1].img)
+			mlx_destroy_image(game->mlx.mlx_ptr, game->texture[1].img);
+		if (game->texture[2].img)
+			mlx_destroy_image(game->mlx.mlx_ptr, game->texture[2].img);
+		if (game->texture[3].img)
+			mlx_destroy_image(game->mlx.mlx_ptr, game->texture[3].img);
 	}
-	if(game->texture)
+	if (game->texture)
 		free(game->texture);
-    if(game->map.north_texture_path)
-        free(game->map.north_texture_path);
-    if(game->map.south_texture_path)
-        free(game->map.south_texture_path);
-    if(game->map.east_texture_path)
-        free(game->map.east_texture_path);
-    if(game->map.west_texture_path)
-        free(game->map.west_texture_path);
-    if(game->map.grid)
-        free_grid(game->map.grid);
-    if (game->mlx.img_ptr)
-        mlx_destroy_image(game->mlx.mlx_ptr, game->mlx.img_ptr);
-    if (game->mlx.win_ptr)
-        mlx_destroy_window(game->mlx.mlx_ptr, game->mlx.win_ptr);
-    if (game->mlx.mlx_ptr)
-    {
-        mlx_destroy_display(game->mlx.mlx_ptr);
-        free(game->mlx.mlx_ptr);
-    }
+	if (game->map.north_texture_path)
+		free(game->map.north_texture_path);
+	if (game->map.south_texture_path)
+		free(game->map.south_texture_path);
+	if (game->map.east_texture_path)
+		free(game->map.east_texture_path);
+	if (game->map.west_texture_path)
+		free(game->map.west_texture_path);
+	if (game->map.grid)
+		free_grid(game->map.grid);
+	if (game->mlx.img_ptr)
+		mlx_destroy_image(game->mlx.mlx_ptr, game->mlx.img_ptr);
+	if (game->mlx.win_ptr)
+		mlx_destroy_window(game->mlx.mlx_ptr, game->mlx.win_ptr);
+	if (game->mlx.mlx_ptr)
+	{
+		mlx_destroy_display(game->mlx.mlx_ptr);
+		free(game->mlx.mlx_ptr);
+	}
 }
 
-void free_string_array(char **array)
+void	free_string_array(char **array)
 {
-    int i;
+	int	i;
 
-    if (array == NULL)
-        return;
-    i = 0;
-    while (array[i] != NULL)
-    {
-        free(array[i]);
-        i++;
-    }
-    free(array);
+	if (array == NULL)
+		return ;
+	i = 0;
+	while (array[i] != NULL)
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
 }
