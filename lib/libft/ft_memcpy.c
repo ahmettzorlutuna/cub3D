5a84cb25c3ab1de316ddc03233254877632f95ec
Ahmet Tayyip Zorlutuna <azorlutu@k1m08s06.42istanbul.com.tr>
2025-11-23 14:43:19 +0300

    refactor(norm): One line space handle

--- lib/libft/ft_memcpy.c
+++ lib/libft/ft_memcpy.c
@@ -9,6 +9,7 @@
 /*   Updated: 2024/10/08 15:25:59 by azorlutu         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
